e19adcf9a24fb18a01702af1dd6cba674290f407
EnockNyamagwa <98160096+EnockNyamagwa@users.noreply.github.com>
2022-09-23 14:36:13 +0300

    Create dataTypes.dart

---
+++ Data Types/dataTypes.dart
@@ -0,0 +1,17 @@
+void main(){
+  bool isAlive = true; //Accepts a TRUE or FALSE value
+  int age = 21; //Accepts whole numbers
+  String name = "Enock"; //Accepts Strings eg names of people, country etc
+  double marks = 80.23; //Accepts floating point numbers ie with decimals
+
+  print(isAlive);
+  print(age);
+  print(name);
+  print(marks);
+}
+
+# Output
+true
+21
+Enock
+80.23
